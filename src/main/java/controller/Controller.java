dde4ab6bc392d577c6036a7df5f40fe1037ad7a1
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-10-26 17:38:56 -0300

    implementing the javaBeans class and updating the HTML

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,26 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet("/Controller")
+@WebServlet(urlPatterns = {"/Controller", "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public Controller() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
--- src/main/java/model/JavaBeans.java
+++ src/main/java/model/JavaBeans.java
@@ -1,5 +1,70 @@
 package model;
 
+import java.util.Objects;
+
 public class JavaBeans {
+	
+	private Integer idcon;
+	private String nome;
+	private String fone;
+	private String email;
+	
+	public JavaBeans() {
+		
+	}
+
+	public JavaBeans(Integer idcon, String nome, String fone, String email) {
+		this.idcon = idcon;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getIdcon() {
+		return idcon;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idcon);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JavaBeans other = (JavaBeans) obj;
+		return Objects.equals(idcon, other.idcon);
+	}
+	
+	
 
 }
